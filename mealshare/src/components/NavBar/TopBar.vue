347bf16d4276bc25650626d19c7c0d6b7b28d753
Lucerno1 <bram_vos@hotmail.com>
2020-04-02 13:24:37 +0200

    Added Top Bar Component

---
+++ mealshare/src/components/NavBar/TopBar.vue
@@ -0,0 +1,17 @@
+<template>
+     <v-toolbar flat color="#74D277">
+            <v-row align="center" justify="center">
+                <v-toolbar-title  class="white--text">{{pageTitle}}</v-toolbar-title>
+            </v-row>
+        </v-toolbar>
+
+</template>
+
+<script>
+export default {
+    name:"TopBar",
+    props:{
+        pageTitle: String,
+    }
+}
+</script>
