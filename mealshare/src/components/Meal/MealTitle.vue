7dee899995e20c087e45cd2500ad7c8eb3da3a25
Niek125 <niek.sleddens@gmail.com>
2020-04-02 13:47:15 +0200

    added mealtitle component

---
+++ mealshare/src/components/Meal/MealTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-col cols="12" class="pa-0">
+        <v-row class="ma-0">
+            <v-card-title class="white--text pt-3 pb-1">Meatballs
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card flat color="#74D277" tile id="shaved-edge">
+                <v-card-title class="white--text py-2">€3,50</v-card-title>
+            </v-card>
+        </v-row>
+    </v-col>
+</template>
+
+<script>
+    export default {
+        name: "MealTitle"
+    }
+</script>
+
+<style scoped>
+    #shaved-edge {
+        border-bottom-left-radius: 19px;
+    }
+</style>
