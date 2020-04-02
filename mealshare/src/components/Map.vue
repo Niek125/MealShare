85ebce0123ae87157dd3a803fd42a88eefbcd8ea
Niek125 <niek.sleddens@gmail.com>
2020-04-02 13:33:25 +0200

    added topbar

--- mealshare/src/components/Map.vue
+++ mealshare/src/components/Map.vue
@@ -1,7 +1,10 @@
 <template>
-    <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded">
-        <MglMarker :coordinates="coordinates" color="#74D277">
-            <MglPopup :closeButton="false" :offset="{
+    <div>
+        <TopBar page-title="Find a meal"></TopBar>
+        <v-sheet height="calc(100vh - 112px)" tile>
+            <MglMap :accessToken="accessToken" :mapStyle="mapStyle" @load="onMapLoaded">
+                <MglMarker :coordinates="coordinates" color="#74D277">
+                    <MglPopup :closeButton="false" :offset="{
                 'top': [0, 0],
                  'top-left': [0,0],
                   'top-right': [0,0],
@@ -11,40 +14,44 @@
                       'left': [markerRadius, (markerHeight - markerRadius) * -1],
                        'right': [-markerRadius, (markerHeight - markerRadius) * -1]
             }">
-                <v-card color="#74D277" flat>
-                    <v-row class="ma-0" justify="center" align="center">
-                        <v-col cols="10" class="pa-0">
-                            <v-row class="ma-0">
-                                <v-card-text class="pa-2 pb-0">{{mealName}}</v-card-text>
-                                <v-row class="ma-0 px-2 pb-1">
-                                    <v-icon small>mdi-alarm</v-icon>
-                                    {{timeSpan}}
-                                    <v-spacer></v-spacer>
-                                    <v-icon small>mdi-currency-eur</v-icon>
-                                    {{price}}
-                                </v-row>
+                        <v-card color="#74D277" flat>
+                            <v-row class="ma-0" justify="center" align="center">
+                                <v-col cols="10" class="pa-0">
+                                    <v-row class="ma-0">
+                                        <v-card-text class="pa-2 pb-0">{{mealName}}</v-card-text>
+                                        <v-row class="ma-0 px-2 pb-1">
+                                            <v-icon small>mdi-alarm</v-icon>
+                                            {{timeSpan}}
+                                            <v-spacer></v-spacer>
+                                            <v-icon small>mdi-currency-eur</v-icon>
+                                            {{price}}
+                                        </v-row>
+                                    </v-row>
+                                </v-col>
+                                <v-col cols="2" class="pa-0">
+                                    <MealOverview>
+                                        <template v-slot:activator="{on}">
+                                            <v-btn icon v-on="on">
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-btn>
+                                        </template>
+                                    </MealOverview>
+                                </v-col>
                             </v-row>
-                        </v-col>
-                        <v-col cols="2" class="pa-0">
-                            <MealOverview>
-                                <template v-slot:activator="{on}">
-                                    <v-btn icon v-on="on">
-                                        <v-icon>mdi-chevron-right</v-icon>
-                                    </v-btn>
-                                </template>
-                            </MealOverview>
-                        </v-col>
-                    </v-row>
-                </v-card>
-            </MglPopup>
-        </MglMarker>
-    </MglMap>
+                        </v-card>
+                    </MglPopup>
+                </MglMarker>
+            </MglMap>
+        </v-sheet>
+    </div>
+
 </template>
 
 <script>
     import Mapbox from "mapbox-gl";
     import {MglMap, MglMarker, MglPopup} from "vue-mapbox";
     import MealOverview from "@/components/MealOverview";
+    import TopBar from "./NavBar/TopBar";
 
     export default {
         name: "Map",
@@ -70,6 +77,7 @@
             this.mapbox = Mapbox;
         },
         components: {
+            TopBar,
             MealOverview,
             MglMap,
             MglMarker,
