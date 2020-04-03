5254ecde8d3030bd048a3094376f4430b53f3d85
Niek125 <niek.sleddens@gmail.com>
2020-04-03 10:22:40 +0200

    added vuex store

--- mealshare/src/main.js
+++ mealshare/src/main.js
@@ -4,11 +4,13 @@ import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
 
 import router from "@/router";
+import store from "./store/store";
 
 Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     vuetify,
     render: h => h(App)
 }).$mount('#app')
